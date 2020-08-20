c92b341b4a77c36fabf0894626cd046a72e75e88
William Findlay <william@williamfindlay.com>
2020-08-20 17:06:23 -0400

    Add proper entrypoint

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='tabot',
     version='0.0.1',
-    url='https://github.com/tabot.git',
+    url='https://github.com/TAbot.git',
     author='William Findlay',
     author_email='william@williamfindlay.com',
     description='A Discord bot to help teaching online university courses.',
     packages=['tabot'],
+    scripts=['bin/tabot'],
 )
