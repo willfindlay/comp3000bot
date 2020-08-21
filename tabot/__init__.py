959c022752c05dc02b1b401348c345bca156664e
William Findlay <william@williamfindlay.com>
2020-08-21 10:11:53 -0400

    Improve help command

--- tabot/__init__.py
+++ tabot/__init__.py
@@ -9,11 +9,17 @@ from tabot.poll import Polls
 from tabot.manage_students import ManageStudents
 from tabot.status_message import StatusMessage
 
+class MyHelpCommand(commands.DefaultHelpCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.width = 300
+
 def main(sys_args = sys.argv[1:]):
     os.makedirs(config.DATA_DIR, exist_ok=True)
     os.makedirs(config.STUDENTS_DIR, exist_ok=True)
 
     client = commands.Bot(command_prefix='!')
+    client.help_command = MyHelpCommand()
 
     client.add_cog(Polls(client))
     client.add_cog(ManageStudents(client))
